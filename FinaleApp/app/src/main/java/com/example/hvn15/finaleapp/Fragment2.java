941aed3a2f3b32f90fe01f29bf8fd2841e72a6d2
Mokayed <marik.3000@hotmail.com>
2018-04-12 14:44:34 +0200

    googlemap+navbar

--- FinaleApp/app/src/main/java/com/example/hvn15/finaleapp/Fragment2.java
+++ FinaleApp/app/src/main/java/com/example/hvn15/finaleapp/Fragment2.java
@@ -15,10 +15,31 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import static android.content.ContentValues.TAG;
 
-public class Fragment2 extends Fragment {
+public class Fragment2 extends Fragment implements OnMapReadyCallback {
 
+    public Fragment2() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     private static final String Tag = "Fragment1";
 
@@ -27,10 +48,9 @@ public class Fragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment2_layout, container, false);
-
-        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
-        btnNavSecondActivity = (Button) view.findViewById(R.id.btnNavSecondActivity);
+        mView = inflater.inflate(R.layout.fragment2_layout, container, false);
+        BottomNavigationView bottomNavigationView = mView.findViewById(R.id.bottom_navigation);
+        btnNavSecondActivity = (Button) mView.findViewById(R.id.btnNavSecondActivity);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -57,6 +77,30 @@ public class Fragment2 extends Fragment {
                 startActivity(intent);
             }
         });
-        return view;
+        return mView;
     }
+    @Override
+    public void onViewCreated(View view, Bundle saveInstanceState){
+        super.onViewCreated(view,saveInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+
+        googleMap.setMapType(googleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(55.676098, 12.568337)).title("Dk").snippet("I Love Dk"));
+        CameraPosition DK= CameraPosition.builder().target(new LatLng(55.676098, 12.568337)).zoom(5).bearing(0).tilt(0).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(DK));
+    }
+
 }
